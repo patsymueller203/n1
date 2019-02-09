7c03b0f28f93b45edd212950c8928c0e7961118c
patsymueller203 <47471982+patsymueller203@users.noreply.github.com>
2019-02-09 08:50:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.redirect("http://clicktrkr.xyz/1wqfry", True)
+        self.redirect("https://www.nnnow.com/", True)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
